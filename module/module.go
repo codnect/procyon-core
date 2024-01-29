ec85f3215f2786dec28d42395cb21922a2ab7425
Burak Koken <burakkokenn@gmail.com>
2024-01-29 20:39:35 +0300

    Add module.Use function to initialize modules

--- module/module.go
+++ module/module.go
@@ -1,5 +1,22 @@
 package module
 
+import "codnect.io/reflector"
+
 type Module interface {
 	InitModule()
+}
+
+func Use[M Module]() {
+	typ := reflector.TypeOf[M]()
+	if reflector.IsStruct(typ) {
+		moduleStruct := reflector.ToStruct(typ)
+		instance, err := moduleStruct.Instantiate()
+
+		if err != nil {
+			panic(err)
+		}
+
+		m := instance.Elem().(Module)
+		m.InitModule()
+	}
 }

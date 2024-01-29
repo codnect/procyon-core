e7946505925059d3da45ea8ff1c14ec45a3f7255
Burak Koken <burakkokenn@gmail.com>
2024-01-29 22:40:55 +0300

    Add default environment customizer to load environment variables and profiles

--- runtime/init.go
+++ runtime/init.go
@@ -1,7 +1,31 @@
 package runtime
 
-import "codnect.io/procyon-core/component"
+import (
+	"codnect.io/procyon-core/component"
+	"codnect.io/procyon-core/component/condition"
+	"codnect.io/procyon-core/runtime/health/availability"
+)
 
 func init() {
-	component.Register(newStartupListener)
+	// runtime
+	component.Register(newStartupListener, component.Name("procyonStartupListener"))
+	component.Register(newEnvironmentCustomizer, component.Name("procyonEnvironmentCustomizer"))
+	//component.Register(NewDefaultLifecycleProcessor, component.Name("lifecycleProcessor")).
+	//	ConditionalOn(condition.OnMissing("lifecycleProcessor"))
+
+	// runtime/health/availability
+	component.Register(availability.NewStateHolder, component.Name("availabilityStateHolder"))
+	component.Register(availability.NewLivenessStateHealthChecker).
+		ConditionalOn(condition.OnMissing("livenessStateHealthChecker")).
+		ConditionalOn(condition.OnProperty("enabled").
+			Prefix("procyon.health.check.livenessstate").
+			HavingValue("true"),
+		)
+
+	component.Register(availability.NewReadinessStateHealthChecker).
+		ConditionalOn(condition.OnMissing("readinessStateHealthChecker")).
+		ConditionalOn(condition.OnProperty("enabled").
+			Prefix("procyon.health.check.readiness").
+			HavingValue("true"),
+		)
 }

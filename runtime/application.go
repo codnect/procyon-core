0955b751726ed06f83b86f62361cc49fbf649c74
Burak Koken <burakkokenn@gmail.com>
2024-08-15 00:05:34 +0300

    Rename Runner interface to ApplicationRunner

--- runtime/application.go
+++ runtime/application.go
@@ -4,3 +4,7 @@ type Application interface {
 	Run(args ...string) error
 	Exit() int
 }
+
+type ApplicationRunner interface {
+	Run(ctx Context, args *Arguments) error
+}
--- runtime/runner.go
+++
@@ -1,5 +0,0 @@
-package runtime
-
-type Runner interface {
-	Run(ctx Context, args *Arguments) error
-}

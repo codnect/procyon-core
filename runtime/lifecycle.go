98e21ffa34ad9403c560ceb50655fdbe5b108150
Burak Koken <burakkokenn@gmail.com>
2024-08-20 00:01:59 +0300

    Add Lifecycle properties and server properties

--- runtime/lifecycle.go
+++ runtime/lifecycle.go
@@ -1,9 +1,23 @@
 package runtime
 
-import "context"
+import (
+	"codnect.io/procyon-core/runtime/property"
+	"context"
+	"time"
+)
 
 type Lifecycle interface {
 	Start(ctx context.Context) error
 	Stop(ctx context.Context) error
 	IsRunning() bool
+}
+
+type LifecycleProperties struct {
+	property.Properties `prefix:"procyon.lifecycle"`
+
+	ShutdownTimeout time.Duration `prop:"shutdown-timeout" default:"30000"`
+}
+
+func NewLifecycleProperties() *ServerProperties {
+	return &ServerProperties{}
 }
--- runtime/server.go
+++ runtime/server.go
@@ -1,9 +1,22 @@
 package runtime
 
-import "context"
+import (
+	"codnect.io/procyon-core/runtime/property"
+	"context"
+)
 
 type Server interface {
 	Start(ctx context.Context) error
 	Stop(ctx context.Context) error
 	Port() int
+}
+
+type ServerProperties struct {
+	property.Properties `prefix:"procyon.server"`
+
+	Port int `prop:"port"`
+}
+
+func NewServerProperties() *ServerProperties {
+	return &ServerProperties{}
 }
